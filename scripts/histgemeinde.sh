68ade6c77888701c00292a1414a589243207c92f
Adrian Gschwend <ktk@netlabs.org>
2019-01-28 16:37:09 +0100

    Add labels for cantons & municipalities

---
+++ scripts/histgemeinde.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -H "Accept: application/n-triples" -o target/municipalities.nt --data-urlencode query@sparql/extra_municipality2currentname.rq http://data.admin.ch/query
+curl -H "Accept: application/n-triples" -o target/cantons.nt --data-urlencode query@sparql/extra_cantonname.rq http://data.admin.ch/query
