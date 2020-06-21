b4d4393c4216b8bf7bfafbf2b7423bc4fd039c3c
Adrian Gschwend <ktk@netlabs.org>
2020-06-21 21:44:24 +0200

    Fix username & pwd

--- scripts/put.sh
+++ scripts/put.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## This is a workaround to put the cube metadata and generate the missing triples for the Cube JS API
-curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD_TEST} \
+curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} \
      --http1.1 \
      -X POST \
      -H Content-Type:text/turtle \
@@ -8,4 +8,4 @@ curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD_TEST} \
      -G https://stardog-test.cluster.ldbar.ch/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/blv/animalpest
 #curl -n -X POST --data timeout=1000000 --data-urlencode query@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/query
-curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD_TEST} -X POST --data timeout=1000000 --data-urlencode update@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/update
+curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} -X POST --data timeout=1000000 --data-urlencode update@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/update
