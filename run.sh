fa0e8316289a259aedad727470f3dcdf1a4acb57
Adrian Gschwend <ktk@netlabs.org>
2020-06-23 19:58:09 +0200

    Fix ENV vars

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ variables=()
 
 [[ -n "$ENDPOINT" ]] && variables+=(--variable="endpoint=$ENDPOINT")
 [[ -n "$STARDOG_USERNAME" ]] && variables+=(--variable="user=$STARDOG_USERNAME")
-[[ -n "$STARDOG_PASSWORD" ]] && variables+=(--variable="password=$STARDOG_PASSWORD")
+[[ -n "$STARDOG_PASSWORD_TEST" ]] && variables+=(--variable="password=$STARDOG_PASSWORD_TEST")
 
 ./node_modules/.bin/barnard59 run -v \
   --format text/turtle \
