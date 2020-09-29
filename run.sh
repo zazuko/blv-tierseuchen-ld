de59c1fbbbd9e91a64c922dd47089f7e0c0e1440
Adrian Gschwend <ktk@netlabs.org>
2020-09-29 11:15:54 +0200

    run & package.json update

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ variables=()
 
 [[ -n "$ENDPOINT" ]] && variables+=(--variable="endpoint=$ENDPOINT")
 [[ -n "$STARDOG_USERNAME" ]] && variables+=(--variable="user=$STARDOG_USERNAME")
-[[ -n "$STARDOG_PASSWORD_TEST" ]] && variables+=(--variable="password=$STARDOG_PASSWORD_TEST")
+[[ -n "$STARDOG_PASSWORD" ]] && variables+=(--variable="password=$STARDOG_PASSWORD")
+[[ -n "$SSH_PRIVATE_KEY" ]] && variables+=(--variable="privateKey=$SSH_PRIVATE_KEY")
 
 ./node_modules/.bin/barnard59 run -v \
   --format text/turtle \
