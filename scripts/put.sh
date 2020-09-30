ed763f2ed6e4c2fd279af8ade67e953f68d3dd81
Adrian Gschwend <ktk@netlabs.org>
2020-09-30 18:27:18 +0200

    Reflect new gitlab variable names

--- scripts/put.sh
+++ scripts/put.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## This is a workaround to put the cube metadata and generate the missing triples for the Cube JS API
-#curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} -X POST  --data timeout=1000000  --data-urlencode update="CLEAR GRAPH <https://linked.opendata.swiss/graph/blv/animalpest> " https://stardog-test.cluster.ldbar.ch/lindas/update
-curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} \
+#curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD} -X POST  --data timeout=1000000  --data-urlencode update="CLEAR GRAPH <https://linked.opendata.swiss/graph/blv/animalpest> " https://stardog-test.cluster.ldbar.ch/lindas/update
+curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD} \
      --http1.1 \
      -X POST \
      -H Content-Type:text/turtle \
@@ -9,4 +9,4 @@ curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} \
      -G https://stardog-test.cluster.ldbar.ch/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/blv/animalpest
 #curl -n -X POST --data timeout=1000000 --data-urlencode query@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/query
-curl -u ${FSVO_USERNAME}:${FSVO_PASSWORD_TEST} -X POST --data timeout=1000000 --data-urlencode update@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/update
+curl -u ${STARDOG_USERNAME}:${STARDOG_PASSWORD} -X POST --data timeout=1000000 --data-urlencode update@sparql/rdf-data-cube-meta-hacklive.rq https://stardog-test.cluster.ldbar.ch/lindas/update
